2f928e5ee51aa13e6a293c8dc7c62cd263487cf8
Allan Binayao <allan.binayao@gmail.com>
2020-05-23 17:54:37 -0500

    components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import About from "./Pages/About/About"
+import Portfolio from "./Pages/Portfolio"
+import Nav from "./Components/Nav"
+import Footer from "./Compenents/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <Nav />
+     <Switch>
+       <Route exact path={["/", "/about"]}>
+         <About />
+       </Route>
+       <Route exact path={"/portfolio"}>
+         <Portfolio />
+       </Route>
+     </Switch>
+     <Footer />
+   </Router>
   );
 }
 
