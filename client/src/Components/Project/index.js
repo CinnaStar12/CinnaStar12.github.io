f89bb3803beb9c30e3424512dc2365349c7724ce
Allan Binayao <allan.binayao@gmail.com>
2020-05-23 19:42:57 -0500

    created project component and changed portfolio page

---
+++ client/src/Components/Project/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./style.css"
+function Project(props) {
+    return (
+        <div class="card mb-3 w-75">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src={props.img} class="card-img" alt={props.title} />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{props.title}</h5>
+                        <p class="card-text">{props.text}</p>
+                        <a href={props.deploy} className="btn btn-primary m-1">Check It Out</a>
+                        <a href={props.repo} className="btn btn-secondary m-1">Repository</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project
--- client/src/Pages/Portfolio.js
+++ client/src/Pages/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Container, Row, Col } from "../Components/Grid"
-import Card from "../Components/Card"
+import Project from "../Components/Project"
 
 const Portfolio = () => {
     return (
@@ -9,10 +9,30 @@ const Portfolio = () => {
                     <Col size="lg">
                         <p className="h3">Projects</p>
                         <Row>
-                            <Card title="Vaendio" img="./assets/images/vaendio.png" href="https://github.com/CinnaStar12/Vaendio"/>
-                            <Card title="kiloMaps" img="./assets/images/kilo.png" href="https://github.com/CinnaStar12/kiloMaps"/>
-                            <Card title="Note-Taker" img="./assets/images/note-taker.png" href="https://github.com/CinnaStar12/Note-Taker"/>
-                            <Card title="Day Planner" img="./assets/images/planner.png" href="https://github.com/CinnaStar12/Day_Planner"/>
+                            <Project 
+                            title="Vaendio"
+                            img="/assets/images/vaendio.png"
+                            text="An app created to help smaller vendors list their places of business (ie. food trucks, farmers markets, road side stands, etc.) and to help users find local goods and services"
+                            deploy="https://vaendio-ajb.herokuapp.com/"
+                            repo="https://github.com/CinnaStar12/Vaendio"/>
+                            <Project 
+                            title="kiloMaps"
+                            img="/assets/images/kilo.png"
+                            text="An app to help you plan out or redesign the layouts of your room. My very first project."
+                            deploy="https://kilomaps.web.app/"
+                            repo="https://github.com/CinnaStar12/kiloMaps"/>
+                            <Project 
+                            title="Note Taker"
+                            img="/assets/images/note-taker.png"
+                            text="My first express app. Allow you to take notes and save them to an express server"
+                            deploy="https://ajb-note-taker.herokuapp.com/"
+                            repo="https://github.com/CinnaStar12/Note-Taker"/>
+                            <Project 
+                            title="Dayplanner"
+                            img="/assets/images/planner.png"
+                            text="A simple app that allow you to plan out a workday. It uses Moment.js in order to tell you what tasks are upcoming and which have already passed"
+                            deploy="https://cinnastar12.github.io/Day_Planner/"
+                            repo="https://github.com/CinnaStar12/Day_Planner"/>
                         </Row>
                     </Col>
                 </Row>
